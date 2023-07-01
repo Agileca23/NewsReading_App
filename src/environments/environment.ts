a46651bca2fb468d4b14002c95a8f18a067af82f
Agileca23 <101391127+Agileca23@users.noreply.github.com>
2023-07-02 00:50:33 +0100

    News app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,34 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+API_KEY:''64a0a47357c2a4.74379516,
+API_URL:'https://eodhistoricaldata.com/financial-apis/'
 export const environment = {
   production: false
 };
+import { environment } from 'src/environments/environment';
+import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
+
+const API_URL = environment.API_URL;
+const API_KEY = environment.API_KEY;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  constructor(public httpClient:HttpClient) {
+    this.loadData()
+  }
+
+  loadData(){
+      this.httpClient.get(`${API_URL}/weather?q=${"Dublin"}&appid=${API_KEY}`).subscribe(results =>{
+        console.log(results);
+    })
+  }
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
